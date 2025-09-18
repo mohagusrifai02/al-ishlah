da16698c3437f6284097476cb867050915992e8c
agus rifai <agus0815@gmail.com>
2025-09-18 11:28:06 +0700

    duration count

--- src/components/AnimatedCounter.tsx
+++ src/components/AnimatedCounter.tsx
@@ -17,7 +17,7 @@ export  function AnimatedCounter() {
 export function SantriPutra(){
     const ref = useRef(null);
     const isInView = useInView(ref, { once: true, amount:0.6});
-    const count = useCounter({ target:25, duration:3000, start:isInView});
+    const count = useCounter({ target:25, duration:2000, start:isInView});
 
     return (
         <div ref={ref} className="santriputra" style={{ fontSize:'100px', fontWeight:'700' }}>
@@ -28,7 +28,7 @@ export function SantriPutra(){
 export function SantriPutri(){
     const ref = useRef(null);
     const isInView = useInView(ref, { once: true, amount:0.6});
-    const count = useCounter({ target:20, duration:3000, start:isInView});
+    const count = useCounter({ target:20, duration:2000, start:isInView});
 
     return (
         <div ref={ref} className="santriputri" style={{ fontSize:'100px', fontWeight:'700' }}>
@@ -39,7 +39,7 @@ export function SantriPutri(){
 export function PengurusPutra(){
     const ref = useRef(null);
     const isInView = useInView(ref, { once: true, amount:0.6});
-    const count = useCounter({ target:10, duration:4000, start:isInView});
+    const count = useCounter({ target:10, duration:2000, start:isInView});
 
     return (
         <div ref={ref} className="pengurusputra" style={{ fontSize:'100px', fontWeight:'700' }}>
@@ -50,7 +50,7 @@ export function PengurusPutra(){
 export function PengurusPutri(){
     const ref = useRef(null);
     const isInView = useInView(ref, { once: true, amount:0.6});
-    const count = useCounter({ target:5, duration:5000, start:isInView});
+    const count = useCounter({ target:5, duration:2000, start:isInView});
 
     return (
         <div ref={ref} className="pengurusputri" style={{ fontSize:'100px', fontWeight:'700' }}>
