5f77f0dd0c5ab43d173bd55187b19922ac20ca3a
agus rifai <agus0815@gmail.com>
2025-09-19 10:56:47 +0700

    ui update

--- src/app/program/page.tsx
+++ src/app/program/page.tsx
@@ -2,6 +2,8 @@
 
 import { motion } from 'framer-motion'
 import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMosque, faSchool, faHandHoldingHand } from '@fortawesome/free-solid-svg-icons';
 
 const Program=()=>{
     return (
@@ -15,13 +17,7 @@ const Program=()=>{
                         transition={{ duration:0.6 }}
                         viewport={{ amount:0.4, once:false }}
                     >
-                        <Image
-                            className='img'
-                            src='/santriwan.png'
-                            alt='santriwan'
-                            width={100}
-                            height={100}
-                        />
+                        <FontAwesomeIcon icon={faMosque} className='img'/>
                         <div className="deskripsi">
                             <h3>Dakwah</h3>
                             <p>Melalui kegiatan dakwah yang inklusif dan berkelanjutan, kami berkomitmen menyebarkan nilai-nilai kebaikan, memperkuat ukhuwah Islamiyah, dan membangun kesadaran spiritual di tengah masyarakat.</p>
@@ -33,13 +29,7 @@ const Program=()=>{
                         transition={{ duration:0.6 }}
                         viewport={{ amount:0.4, once:false }}
                     >
-                        <Image 
-                            className='img'
-                            src='/santriwati.png'
-                            alt='santriwati'
-                            width={100}
-                            height={100}
-                        />
+                        <FontAwesomeIcon icon={faHandHoldingHand} className='img'/>
                         <div className="deskripsi">
                             <h3>Sosial</h3>
                             <p>Yayasan Al-Ishlah berkomitmen menghadirkan kepantian yang layak dan penuh kasih bagi anak-anak yatim, piatu, dan dhuafa. Melalui pendidikan, pembinaan karakter, dan pemenuhan kebutuhan dasar.</p>
@@ -51,13 +41,7 @@ const Program=()=>{
                         transition={{ duration:0.6 }}
                         viewport={{ amount:0.4, once:false }}
                     >
-                        <Image 
-                            className='img'
-                            src='/siswa.png'
-                            alt='siswa'
-                            width={100}
-                            height={100}
-                        />
+                        <FontAwesomeIcon icon={faSchool} className='img'/>
                         <div className="deskripsi">
                             <h3>Pendidikan</h3>
                             <p>Membuka akses pendidikan berkualitas yang berlandaskan nilai-nilai Islam, Yayasan Al-Ishlah membina generasi muda agar tumbuh menjadi insan berilmu, berakhlak, dan siap berkontribusi bagi masyarakat.</p>
